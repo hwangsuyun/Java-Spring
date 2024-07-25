e5a1526ea7e15f1e5d2bcb1f3fd375b35237da95
hwangsuyun@naver.com <hwangsuyun@naver.com>
2024-07-25 19:23:10 +0900

    MyJsonTests.java (2024.07.25 syhwang)

---
+++ src/test/java/com/example/MyJsonTests.java
@@ -0,0 +1,36 @@
+package com.example;
+
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.json.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.json.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@JsonTest
+public class MyJsonTests {
+    @Autowired
+    private JacksonTester<VehicleDetails> json;
+
+    @Test
+    void testSerialize() throws Exception {
+        VehicleDetails details = new VehicleDetails("Honda", "Civic");
+        // Assert against a `.json` file in the same package as the test
+        assertThat(this.json.write(details)).isEqualToJson("expected.json");
+        // Or use JSON path based assertions
+        assertThat(this.json.write(details)).hasJsonPathStringValue("@.make");
+        assertThat(this.json.write(details)).extractingJsonPathStringValue("@.make")
+                .isEqualTo("Honda");
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        String content = "{\"make\":\"Ford\",\"model\":\"Focus\"}";
+        assertThat(this.json.parse(content))
+                .isEqualTo(new VehicleDetails("Ford", "Focus"));
+        assertThat(this.json.parseObject(content).getMake()).isEqualTo("Ford");
+    }
+}
+*/
